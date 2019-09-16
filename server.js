6ed0eacd4bded5269ac05fce79bdc0fc20467224
chiomadans1759 <oluchukwuokpala1759@gmail.com>
2019-09-16 22:47:39 +0100

    Made changes

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+
+app.use(express.static(path.join(__dirname, './public'))); 
+app.all('*', (req, res) => res.sendFile(path.join(__dirname, './public/index.html')));
+
+const port = parseInt(process.env.PORT, 10) || 3000;
+app.listen(port, () => console.log(`Listening on port ${port}`));
+
